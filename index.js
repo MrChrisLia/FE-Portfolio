d1946c874073a3aa7cf6a2bf78b449b735b95bc3
Christopher L <60084418+MrChrisLia@users.noreply.github.com>
2023-03-19 01:02:25 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ h7Button.addEventListener("click", () => {
     h7Element.style.display = "none";
     isH7TextVisible = false;
   } else {
-    const text = "<ul><li>- Manage a Web Vulnerabilitiy Scanner and confirm potential vulnerabilities. </li><li>- Manage an Web Application Firewall.</li> <li>- Perform web penetration tests to to find potential vulnerabilities.</li><li>- Work with the SOC team during investigations</li></ul>";
+    const text = "<ul><li>☑️ Manage a Web Vulnerabilitiy Scanner and confirm potential vulnerabilities. </li><li>☑️ Manage an Web Application Firewall.</li> <li>☑️ Perform web penetration tests to to find potential vulnerabilities.</li><li>☑️ Work with the SOC team during investigations</li></ul>";
     h7Element.innerHTML = text;
     h7Element.style.display = "block";
     isH7TextVisible = true;
@@ -30,7 +30,7 @@ luantaButton.addEventListener("click", () => {
         luantaElement.style.display = "none";
         isLuantaTextVisible = false;
     } else {
-        const text = "<ul><li>- Monitor server system health. </li><li>- Provide tier 1 investigations on servers.</li> <li>- Act as first line customer and product support.</li><li>- Work in a 24/7 shift schedule.</li></ul>";
+        const text = "<ul><li>☑️ Monitor server system health. </li><li>☑️ Provide tier 1 investigations on servers.</li> <li>☑️ Act as first line customer and product support.</li><li>☑️ Work in a 24/7 shift schedule.</li></ul>";
         luantaElement.innerHTML = text;
         luantaElement.style.display = "block";
         isLuantaTextVisible = true;
@@ -42,7 +42,7 @@ if (isHessTextVisible) {
     hessElement.style.display = "none";
     isHessTextVisible = false;
 } else {
-    const text = "<ul><li>- Instruct students aging from 2 to 15 in English language. </li><li>- Plan yearly events.</li> <li>- Prepare students to take English exams for overseas school admission.</li><li>- Manage Teacher-Student-Parent relations.</li></ul>";
+    const text = "<ul><li>☑️ Instruct students aging from 2 to 15 in English language. </li><li>☑️ Plan yearly events.</li> <li>☑️ Prepare students to take English exams for overseas school admission.</li><li>☑️ Manage Teacher-Student-Parent relations.</li></ul>";
     hessElement.innerHTML = text;
     hessElement.style.display = "block";
     isHessTextVisible = true;
